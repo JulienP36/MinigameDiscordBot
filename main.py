724421d6e2a27f1d08f50adca5913a6def8b5567
JulienP36 <juju.poitreau@gmail.com>
2020-09-10 10:25:53 +0200

    Nettoyage de code inutile et ajout de quelques commentaires + modification du retour pour afficher le gagnant, maintenant la ligne gagnante passe d'une ligne de cercles verts à une ligne de carrés de la couleur du joueur gagnant.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import traceback
 import discord
 
+#Différentes variables globales nécessaires pour le bon fonctionnement du bot
 game_selected = "none"
 intro = 0
 compteur = 0
@@ -12,10 +13,12 @@ msg_lock = 0
 client = discord.Client()
 Grille = [[0] * 8 for z in range(8)]
 
+#Fonction asynchrone permettant uniquement d'afficher dans la console l'id du bot dès que celui-ci est prêt
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+#Fonction asynchrone permettant de faire le lien entre l'utilisateur et le bot lorsque l'utilisateur envoi un message
 @client.event
 async def on_message(message):
     global game_selected
@@ -40,14 +43,8 @@ async def on_message(message):
             compteur = 0
             msg_lock = 0
             await message.channel.send("Game reset !")
-'''
-@client.event
-async def edit_message(texte):
-    global message_bot
-    try:
-        msg = await channel.fetch_message(message_bot)
-    except Exception as e:
-        print(e)'''
+
+#Fonction asynchrone servant de menu pour lancer les différents jeux
 @client.event
 async def menu(message):
     global game_selected
@@ -65,29 +62,34 @@ async def menu(message):
 #Fonction qui va effectuer le balayage de la grille de puissance 4 pour vérifier si l'un des joueurs est victorieux
 def puissance4_winnerCheck():
     global Grille
+    if (compteur - 1) % 2 == 0:
+        num = 4
+    else:
+        num = 3
     for x in range(5):
         for y in range(8):
             if Grille[x][y] == Grille[x+1][y] == Grille[x+2][y] == Grille[x+3][y] in (1, 2):
-                Grille[x][y], Grille[x+1][y], Grille[x+2][y], Grille[x+3][y] = 3, 3, 3, 3
+                Grille[x][y], Grille[x+1][y], Grille[x+2][y], Grille[x+3][y] = num, num, num, num
                 return "yes"
     for x in range(8):
         for y in range(5):
             if Grille[x][y] == Grille[x][y+1] == Grille[x][y+2] == Grille[x][y+3] in (1, 2):
-                Grille[x][y], Grille[x][y+1], Grille[x][y+2], Grille[x][y+3] = 3, 3, 3, 3
+                Grille[x][y], Grille[x][y+1], Grille[x][y+2], Grille[x][y+3] = num, num, num, num
                 return "yes"
     for x in range(5):
         for y in range(5):
             if Grille[x][y] == Grille[x+1][y+1] == Grille[x+2][y+2] == Grille[x+3][y+3] in (1, 2):
-                Grille[x][y], Grille[x+1][y+1], Grille[x+2][y+2], Grille[x+3][y+3] = 3, 3, 3, 3
+                Grille[x][y], Grille[x+1][y+1], Grille[x+2][y+2], Grille[x+3][y+3] = num, num, num, num
                 return "yes"
     for x in range(5):
         for y in range(5):
             if Grille[x][y] == Grille[x+1][y-1] == Grille[x+2][y-2] == Grille[x+3][y-3] in (1, 2):
-                Grille[x][y], Grille[x+1][y-1], Grille[x+2][y-2], Grille[x+3][y-3] = 3, 3, 3, 3
+                Grille[x][y], Grille[x+1][y-1], Grille[x+2][y-2], Grille[x+3][y-3] = num, num, num, num
                 return "yes"
     return "no"
 
-#Todo: Changer couleur des joueurs
+#Fonction qui va se lancer afin de permettre aux utilisateur de jouer à puissance4
+#Todo: Donner la possibilité de changer la couleur des joueurs
 @client.event
 async def puissance4(message):
     global intro
@@ -123,7 +125,7 @@ async def puissance4(message):
                             Grille[x][column-1] = (compteur % 2) + 1
                             break
                     gagnant = puissance4_winnerCheck()
-                    for i in range(8):
+                    for i in range(8): #Composition du message à envoyer sous forme d'un seul et même message
                         message_grille = message_grille + "\n|"
                         for j in range(8):
                             if Grille[i][j] == 0:
@@ -133,7 +135,9 @@ async def puissance4(message):
                             elif Grille[i][j] == 2:
                                 message_grille = message_grille + ":red_circle:|"
                             elif Grille[i][j] == 3:
-                                message_grille = message_grille + ":green_circle:|"
+                                message_grille = message_grille + ":yellow_square:|"
+                            elif Grille[i][j] == 4:
+                                message_grille = message_grille + ":red_square:|"
                     compteur += 1
                     msg_bot = await message.channel.fetch_message(message_bot)
                     if compteur >= 64:
@@ -167,7 +171,7 @@ async def puissance4(message):
             msg_error = await message.channel.send("Aucun argument donné, ou celui-ci est incorrect\nrappel du format : `choix [1-8]`")
             await msg_error.delete(delay=5)
             print(e)
-            traceback.print_exc()
+            #traceback.print_exc() #utile mais seulement pour debug, à supprimer une fois le code validé
         msg_joueur = await message.channel.fetch_message(msg_id)
         await msg_joueur.delete(delay=1)
 
